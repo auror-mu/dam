87219a0191ca8f5ebf127529e638be0570c5950c
aurora muggianu <auroraemu@gmail.com>
2024-02-02 15:54:53 +0100

    server.js MIME added

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Your static site folder name
-app.use(express.static("dam"));
+app.use(express.static("dam", {
+    setHeaders: (res, path) => {
+        if (path.endsWith('.css')) {
+            res.setHeader('Content-Type', 'text/css');
+        }
+    },
+}));
 
 app.use("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'dam.html'));
@@ -16,3 +22,4 @@ app.use("/", (req, res) => {
 app.listen(port, () => {
     console.log("Server is running on port " + port);
 });
+
