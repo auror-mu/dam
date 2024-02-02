8ae872d3caf384dc7315cf977fa0f018a46c1c5c
aurora muggianu <auroraemu@gmail.com>
2024-02-02 13:56:47 +0100

    server.js added

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Your static site folder name
+app.use(express.static("dam"));
+
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'dam.html'));
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port " +port);
+});
